e9a680da40f1bc318a59d333625c135d13b9d9da
D-Coder135 <devansh1352000@gmail.com>
2023-04-05 15:19:19 +0530

    add some comment as the description inside the code

--- src/main/java/com/decoder135/demo/rest/StudentRestController.java
+++ src/main/java/com/decoder135/demo/rest/StudentRestController.java
@@ -6,4 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+
+    // define an endpoint for "/students" - return a list of students
 }
