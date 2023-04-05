6629d051bed73adb2f1ad6d771615746bcf94bc1
D-Coder135 <devansh1352000@gmail.com>
2023-04-05 15:33:37 +0530

    add the @PostConstruct annotation

--- src/main/java/com/decoder135/demo/rest/StudentRestController.java
+++ src/main/java/com/decoder135/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.decoder135.demo.rest;
 
 import com.decoder135.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class StudentRestController {
 
     // define @PostConstruct to load the student data...only once!
 
+    @PostConstruct
     public void loadStudentData() {
 
     }
