4e5e8e986913d54dbfa833d7f6e6ca196285916e
D-Coder135 <devansh1352000@gmail.com>
2023-04-05 15:20:46 +0530

    add the @GetMapping annotation

--- src/main/java/com/decoder135/demo/rest/StudentRestController.java
+++ src/main/java/com/decoder135/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.decoder135.demo.rest;
 
 import com.decoder135.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class StudentRestController {
 
     // define an endpoint for "/students" - return a list of students
 
+    @GetMapping("/students")
     public List<Student> getStudents() {
         return null;
     }
