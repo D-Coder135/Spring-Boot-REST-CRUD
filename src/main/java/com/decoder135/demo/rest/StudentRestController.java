01d57d8bc51e9e5e1980aa44ee6bb8c8f8253f45
D-Coder135 <devansh1352000@gmail.com>
2023-04-05 15:23:07 +0530

    update the return statement

--- src/main/java/com/decoder135/demo/rest/StudentRestController.java
+++ src/main/java/com/decoder135/demo/rest/StudentRestController.java
@@ -22,6 +22,6 @@ public class StudentRestController {
         theStudents.add(new Student("Vishad", "Vashishtha"));
         theStudents.add(new Student("Ashutosh", "Chaubey"));
 
-        return null;
+        return theStudents;
     }
 }
