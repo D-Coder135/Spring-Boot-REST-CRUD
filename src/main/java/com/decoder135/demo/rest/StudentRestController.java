26f496743dbcbb3391b8b4cc9158e9ef5316836f
D-Coder135 <devansh1352000@gmail.com>
2023-04-05 15:20:12 +0530

    add a public method getStudents() of type List<Student>

--- src/main/java/com/decoder135/demo/rest/StudentRestController.java
+++ src/main/java/com/decoder135/demo/rest/StudentRestController.java
@@ -1,11 +1,18 @@
 package com.decoder135.demo.rest;
 
+import com.decoder135.demo.entity.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
     // define an endpoint for "/students" - return a list of students
+
+    public List<Student> getStudents() {
+
+    }
 }
