b93f7727dbbc24ddda604a388bff718c2017c307
D-Coder135 <devansh1352000@gmail.com>
2023-04-05 15:38:26 +0530

    add a return statement

--- src/main/java/com/decoder135/demo/rest/StudentRestController.java
+++ src/main/java/com/decoder135/demo/rest/StudentRestController.java
@@ -38,6 +38,9 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudentDetailsById(@PathVariable int studentId) {
+
         // just index into the list...keep it simple for now
+
+        return theStudents.get(studentId);
     }
 }
